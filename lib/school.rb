c34cc4eacfdce8896ebaa84ca9ddf5fde4f63183
Silas Michael Knight <silasfelinus@gmail.com>
2020-11-25 01:56:05 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -5,5 +5,18 @@ class School
   @roster = {}
   end
 
+  def add_student(name, grade)
+    if !roster.key?(grade)
+      roster[grade] = []
+    end
+    roster[grade] << nameend
+  end
+
+  def grade(grade)
+    roster[grade]
+  end
+
+  def sort()
+    roster.sort
 
 end
